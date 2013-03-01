3dadabace71eddb18084892dccba68c27edf3f02
the_brat_85 <the_brat_85@f79fa7a2-7851-4206-aa00-5d182cfa6cd6>
2013-03-01 19:21:02 +0000

    Init Import

---
+++ trunk/graffy-ext/armadillo_ext.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   armadillo_ext.h
+ * Author: bharath
+ *
+ * Created on 1 March, 2013, 10:18 PM
+ */
+
+#ifndef ARMADILLO_EXT_H
+#define	ARMADILLO_EXT_H
+
+
+
+#endif	/* ARMADILLO_EXT_H */
+
---
+++ trunk/graffy-ext/octave_ext.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   octave_ext.h
+ * Author: bharath
+ *
+ * Created on 1 March, 2013, 10:19 PM
+ */
+
+#ifndef OCTAVE_EXT_H
+#define	OCTAVE_EXT_H
+
+
+
+#endif	/* OCTAVE_EXT_H */
+
