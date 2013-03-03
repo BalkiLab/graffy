519325e03c33f9078d5179244a535603a8851334
sudip02 <sudip02@f79fa7a2-7851-4206-aa00-5d182cfa6cd6>
2013-03-03 20:05:24 +0000

    Many changes to graph_properties file and added graph name in the extracted sub-graph in graph_operations

--- trunk/graph_properties.h
+++ trunk/graph_properties.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   graph_properties.h
  * Author: bharath
  *
@@ -14,19 +14,27 @@
 using namespace std;
 
 
-namespace CDLib 
+namespace CDLib
 {
     template <typename T>
-    void get_degree_sequence(const graph& g,vector<T>& degrees, bool in_degrees);
+    void get_degree_sequence(const graph& g,vector<T>& degrees, bool in_degrees)
+    {
+        degrees.clear();
+        degrees.assign(g.get_num_nodes(),0);
+        for(id_type i=0;i<g.get_num_nodes();i++)
+            degrees[i] = (in_degrees)? g.get_node_in_degree(i) : g.get_node_out_degree(i);
+    }
     void get_degree_histogram(const graph& g,vector<id_type>& dist, bool in_degrees);
     double get_degree_distribution(const graph& g,vector<double>& dist,bool in_degrees);
+    double get_excess_degree_distribution(const graph& g,vector<double>& dist, bool in_degrees);
     double get_degree_variance(const graph& g,bool in_degrees);
-    void get_degree_assortativity_coefficient(const graph& g,bool in_degrees,vector<double>& assortativity);
-    double get_degree_assortativity_coefficient(const graph& g,bool in_degrees);
+    double get_degree_assortativity_coefficient(const graph& g,vector<double>& assortativity);
+    double unbiased_assortativity(const graph& g);
+    double get_degree_assortativity_coefficient(const graph& g);
     double get_rich_club_coefficient(const graph& g,id_type start_hub_degree);
     double normalized_rich_club_coefficient(const graph& g,id_type start_hub_degree_def);
     double get_poor_club_coefficient(const graph& g,id_type start_hub_degree);
-    
+
     double kl_divergence_from_random_graph(const graph& g);
     double distance_from_random_graph(const graph& g, bool hellinger);  // Hellinger and Bhattacharyya Distance of Degree Distribution
     double connectivity_entropy(const graph& g);
